e7583f9c1884241675b39f2a5e03a92db05a5993
digitalronin <david@digitalronin.com>
2022-03-23 11:59:15 +0900

    Add code to find deployed contract address

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,22 @@ import {useEthers} from "@usedapp/core"
 import {Header} from "./components/Header"
 import {DisplayMessage} from "./components/DisplayMessage"
 import {UpdateMessage} from "./components/UpdateMessage"
+import mapJson from "./contract-data/deployments/map.json"
 
 function App() {
-  const {account} = useEthers()
+  const {account, chainId} = useEthers()
+
+  function getSimpleStorageContractAddress(chainId: string | number | undefined, mapJson: any) {
+    if (chainId === undefined) {
+      return "0x0000000000000000000000000000000000000000"
+    } else {
+      const lookup = chainId == 1337 ? "dev" : String(chainId)
+      return mapJson[lookup]["SimpleStorage"][0]
+    }
+  }
+
+  console.log(chainId)
+  console.log("contract", getSimpleStorageContractAddress(chainId, mapJson))
 
   return (
     <div>
