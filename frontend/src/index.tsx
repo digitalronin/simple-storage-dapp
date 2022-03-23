79e2c058b34bbf81ff11bf0050fe7394d3e245b1
digitalronin <david@digitalronin.com>
2022-03-23 11:14:42 +0900

    Wrap the app with DAppProvider

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {DAppProvider} from "@usedapp/core"
 import './index.css';
 import App from './App';
 
+const config = {
+  multicallAddresses: {
+    "1337": "0x0000000000000000000000000000000000000000",
+  },
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
