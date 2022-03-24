b0f4876b18f268fa015bb10a0ab9a33b2acc454a
digitalronin <david@digitalronin.com>
2022-03-24 11:31:28 +0900

    Use material-ui Card to display the message

--- frontend/src/components/DisplayMessage.tsx
+++ frontend/src/components/DisplayMessage.tsx
@@ -1,15 +1,26 @@
+import {Card, CardContent, Typography, makeStyles} from "@material-ui/core"
 import {useGetMessage} from "../hooks/useGetMessage"
 
 export interface DisplayMessageProps {
   contractAddress: string
 }
 
+const useStyles = makeStyles((theme) => ({
+  spaced: {
+    margin: theme.spacing(2)
+  }
+}))
+
 export const DisplayMessage = ({contractAddress}: DisplayMessageProps) => {
+  const classes = useStyles()
   const message = useGetMessage(contractAddress)
 
   return (
-    <div>
-      DisplayMessage: {message}
-    </div>
+    <Card className={classes.spaced}>
+      <CardContent>
+        Message
+        <Typography variant="h5" component="div">{message}</Typography>
+      </CardContent>
+    </Card>
   )
 }
