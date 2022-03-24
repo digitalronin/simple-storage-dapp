3ba27f01398be5277ec5b0d89a28006db6f04758
digitalronin <david@digitalronin.com>
2022-03-24 10:02:58 +0900

    Enable deployment to the Rinkeby network

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,22 @@
-from brownie import accounts
+from brownie import accounts, network, config
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local", "ganache-ui"]
 
 
-def get_account():
-    return accounts[0]  # TODO make this more flexible
+def get_account(index=None, id=None):
+    net_name = network.show_active()
+
+    if index:
+        account = accounts[index]
+    elif id:
+        account = accounts.load(id)
+    elif(
+        net_name in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or net_name in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        account = accounts[0]
+    else:
+        account = accounts.add(config["wallets"]["from_key"])
+
+    return account
