fe5b9eb7b23b003ccce67919f23161a907f0052c
digitalronin <david@digitalronin.com>
2022-03-24 11:06:32 +0900

    Track the state of the updateMessage TX

--- frontend/src/hooks/useUpdateMessage.ts
+++ frontend/src/hooks/useUpdateMessage.ts
@@ -1,3 +1,4 @@
+import {useEffect, useState} from "react"
 import {utils} from "ethers"
 import {useContractFunction} from "@usedapp/core"
 import {Contract} from "@ethersproject/contracts"
@@ -7,11 +8,25 @@ export const useUpdateMessage = (contractAddress: string) => {
   const ssInterface = new utils.Interface(SimpleStorage.abi)
   const ssContract = new Contract(contractAddress, ssInterface)
 
-  const {send: updateMessageSend} = useContractFunction(ssContract, "update", {transactionName: "Update Message"})
+  const {send: updateMessageSend, state: updateMessageState} = useContractFunction(ssContract, "update", {transactionName: "Update Message"})
+
+  const [state, setState] = useState(updateMessageState)
 
   const updateMessage = (message: string) => {
     return updateMessageSend(message)
   }
 
-  return {updateMessage, state: "Update message state"}
+  useEffect(() => {
+    setState(updateMessageState)
+
+    if (updateMessageState.status === "Success") {
+      console.log("message updated")
+    } else {
+      if (updateMessageState.errorMessage !== undefined) {
+        console.log(`error: ${updateMessageState.errorMessage}`)
+      }
+    }
+  }, [updateMessageState])
+
+  return {updateMessage, state}
 }
